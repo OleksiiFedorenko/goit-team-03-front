83c37f809ff57598e84cc05891e5bb92c897f6a2
Olenteva74 <olenteva74@ukr.net>
2023-08-02 20:01:48 +0300

    Create Home.jsx

---
+++ src/services/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn, selectIsRefreshing } from "store/auth/selectors";
+
+
+
+export const PrivateRoute = ({children}) => {
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+    const isRefreshing = useSelector(selectIsRefreshing);
+
+    if (!isLoggedIn && !isRefreshing) {
+        return <Navigate to="/"/>
+    }
+   return children;
+}
