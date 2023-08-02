ebde44701d227ef15109c2af3d89c96cb8acc76c
OleksiiFedorenko <113383083+OleksiiFedorenko@users.noreply.github.com>
2023-08-02 21:32:53 +0000

    Deploying to gh-pages from @ OleksiiFedorenko/goit-team-03-front@7c576784b39a8fac69a5a1ef9ba841d1b85d894b 🚀

---
+++ static/js/415.6c90dea9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,r,t){t.r(r);var n=t(1087),c=t(3329);r.default=function(){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)("h1",{children:"Welcome"}),(0,c.jsx)(n.rU,{to:"/auth/register",children:"Sign up"}),(0,c.jsx)(n.rU,{to:"/auth/login",children:"Sign in"})]})}}}]);
+//# sourceMappingURL=415.6c90dea9.chunk.js.map
