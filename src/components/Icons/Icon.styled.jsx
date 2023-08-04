e37cffba0505571e2b8801e832e610bce6001bce
Olenteva74 <olenteva74@ukr.net>
2023-08-04 20:09:47 +0300

    add svg-sprite

---
+++ src/components/Icons/Icon.styled.jsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const IconStyled = styled.svg`
+width: 18px;
+height: 18px;
+display: inline-block;
+stroke-width: 0;
+stroke: grey;
+fill: transparent;
+:hover {
+    stroke: currentColor;
+}
+`;
