56e4697ff5b96e2d94315028f813f8219472608e
Olexander Zakatov <sashazakatov17@gmail.com>
2023-08-06 16:39:42 +0300

    update AuthFormLogin.jsx and AuthFormLogin.module.css

--- src/components/AuthFormLogin/AuthFormLogin.jsx
+++ src/components/AuthFormLogin/AuthFormLogin.jsx
@@ -1,52 +1,106 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import * as yup from 'yup';
 import { getLogin } from 'store/auth/operations';
-import { Formik, Field, Form } from 'formik';
+import { Button, ThemeProvider, TextField, OutlinedInput, InputAdornment, IconButton } from '@mui/material';
+import { lightTheme } from '../../themes/themes'
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useFormik } from 'formik';
+import style from './AuthFormLogin.module.css'
 
-import styles from './AuthFormLogin.module.css'
-
-const schema = yup.object().shape({
+const validationSchema = yup.object().shape({
   email: yup.string().email().required(),
   password: yup.string().min(8).required(),
 });
 
-const initialValues={ 
-  email: '', 
-  password: '' 
-}
-
 export const AuthFormLogin = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
   const dispatch = useDispatch();
 
-  const onSubmit = (values) => {
-    console.log(values);
+  const onSubmit = (values, {setSubmitting}) => {
+    alert(JSON.stringify(values, null, 2));
     dispatch(getLogin(values))
+    setSubmitting(false);
   }
 
-  return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={schema}
-      onSubmit={onSubmit}
-    >
-      <Form className={styles.form}>
-        <Field
-          className={styles.input} 
-          id="email"
-          name="email" 
-          type="email"
-          placeholder="Enter your email"
-        />
-        <Field
-          className={styles.input}
-          id="password"
-          name="password" 
-          type="password"
-          placeholder="Confirm a password"
-        />
+  const initialValues = {
+    email: '', 
+    password: '', 
+  } 
 
-        <button className={styles.button} type="submit">Log In Now</button>
-      </Form>
-    </Formik>
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit,
+  });
+
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <form onSubmit={formik.handleSubmit} className={style.form}>
+        {formik.touched.email && formik.errors.email ? (
+          <div className={style.error}>{formik.errors.email.toWellFormed()}</div>
+        ) : null}
+        <TextField
+          name="email"
+          type="email"
+          variant="outlined"
+          placeholder="Enter your email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          fullWidth
+          inputProps={{
+            style: { color: 'white' },
+          }}
+          sx={{
+            marginBottom: '14px',
+            "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+              borderColor: "#BEDBB0",
+            },
+            "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+              borderColor: "#BEDBB0",
+            },
+        }}
+      />
+        {formik.touched.password && formik.errors.password ? (
+          <div className={style.error}>{formik.errors.password}</div>
+        ) : null}
+        <OutlinedInput
+          name="password"
+          type={showPassword ? "text" : "password"}
+          placeholder="Create a password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          fullWidth
+          inputProps={{
+            style: { color: 'white' },
+          }}
+          sx={{
+            marginBottom: '24px',
+            "& .MuiOutlinedInput-notchedOutline": {
+              borderColor: "#BEDBB0",
+            },
+            "&:hover .MuiOutlinedInput-notchedOutline": {
+              borderColor: "#BEDBB0",
+            },
+          }}
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton onClick={handleClickShowPassword} edge="end" style={{color: '#FFF'}}>
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+        <Button color="primary" variant="contained" fullWidth type="submit">
+          Log In Now
+        </Button>
+      </form>
+    </ThemeProvider>
   );
-};
+}
