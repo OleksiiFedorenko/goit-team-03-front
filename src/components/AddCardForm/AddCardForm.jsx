1fc7f1006fc16747efd4c21feae855999e692541
Vadim Yatskul <vadimajj@gmail.com>
2023-08-07 17:33:48 +0300

    commit3

--- src/components/AddCardForm/AddCardForm.jsx
+++ src/components/AddCardForm/AddCardForm.jsx
@@ -1,21 +1,59 @@
-import { useState } from 'react';
 import { Typography, Stack } from '@mui/material';
+import * as Yup from 'yup';
+import { Formik, Form } from 'formik';
+import { Container } from '@mui/material';
+import Textfield from '../FormsUI/TextField';
+import SubmitButton from 'components/FormsUI/SubmitButton';
 
-const initialFieldValues = {
-  id: '',
+const initialValues = {
   title: '',
   description: '',
-  labelColor: '',
-  deadline: new Date(),
+};
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required('Required'),
+  description: Yup.string().required('Required'),
+});
+
+const handleSubmit = values => {
+  console.log(values);
+  //setSubmitting(false);
+  // resetForm();
+  //onCloseModal();
 };
 
 const AddCardForm = () => {
-  const [values, setValues] = useState(initialFieldValues);
-
   return (
-    <Stack>
-      <Typography>Add Card</Typography>;
-    </Stack>
+    <Container>
+      <Formik
+        initialValues={{ ...initialValues }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <Typography variant="h2" component="h2" mb={3}>
+            Add card
+          </Typography>
+          <Textfield
+            name="title"
+            label="Title"
+            sx={{
+              marginBottom: '14px',
+            }}
+          />
+          <Textfield
+            name="description"
+            label="Description"
+            inputProps={{
+              style: {
+                height: '154px',
+              },
+            }}
+          />
+          <SubmitButton>Add</SubmitButton>
+        </Form>
+      </Formik>
+    </Container>
   );
 };
 export default AddCardForm;
