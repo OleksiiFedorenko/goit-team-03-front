0fc36050b0978c0112590c3f19222bee19ff1b3e
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2023-08-09 16:23:19 +0200

    AuthLayout primary commit without MUI Tabs

--- src/components/AuthLayout/AuthLayout.jsx
+++ src/components/AuthLayout/AuthLayout.jsx
@@ -1,19 +1,71 @@
 import { Suspense } from 'react';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Loader } from 'components/Loader/Loader';
+import {
+  Box,
+  Container,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from '@mui/material';
+import { button, container } from 'styles';
 
 export const AuthLayout = () => {
   return (
-    <div>
-      <Link to={'register'}>Registration</Link>
-      <Link to={'login'}>Log in</Link>
+    <Container sx={container.gradient}>
+      <Box sx={container.auth}>
+        <List sx={button.authTabList}>
+          <ListItem sx={button.authTabItem}>
+            <ListItemButton
+              component={NavLink}
+              to="register"
+              sx={button.authTabBtn}
+              style={({ isActive }) =>
+                isActive
+                  ? {
+                      color: '#FFF',
+                    }
+                  : null
+              }
+            >
+              <ListItemText
+                sx={{ p: 0, m: 0 }}
+                primary="Registration"
+                disableTypography
+              />
+            </ListItemButton>
+          </ListItem>
 
-      <ToastContainer theme="colored" />
-      <Suspense fallback={<Loader/>}>
-        <Outlet />
-      </Suspense>
-    </div>
+          <ListItem sx={button.authTabItem}>
+            <ListItemButton
+              component={NavLink}
+              to="login"
+              sx={button.authTabBtn}
+              style={({ isActive }) =>
+                isActive
+                  ? {
+                      color: '#FFF',
+                    }
+                  : null
+              }
+            >
+              <ListItemText
+                sx={{ p: 0, m: 0 }}
+                primary="Log in"
+                disableTypography
+              />
+            </ListItemButton>
+          </ListItem>
+        </List>
+
+        <ToastContainer theme="colored" />
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </Box>
+    </Container>
   );
 };
