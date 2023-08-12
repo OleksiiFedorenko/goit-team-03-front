339b2b1dc117cc6ecbb9dc93d28f9fefda4d7a8b
Denys07748 <denis07748@gmail.com>
2023-08-12 20:58:49 +0300

    add color to theme

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -4,11 +4,13 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import { useState } from 'react';
+import { TextField, useTheme } from '@mui/material';
+import { calendar } from 'styles';
 import { Icon } from 'components/Icons';
-import { TextField } from '@mui/material';
 
 export const Calendar = ({ parentState, initial }) => {
   dayjs().format();
+  const theme = useTheme();
 
   const [dateDeadline, setDateDeadline] = useState(initial ? initial : '');
 
@@ -17,6 +19,8 @@ export const Calendar = ({ parentState, initial }) => {
   const isPastDate = date => {
     return date < currentDate;
   };
+
+  console.log('Theme', theme);
 
   const isToday = date => {
     return (
@@ -33,6 +37,8 @@ export const Calendar = ({ parentState, initial }) => {
           views={['month', 'day']}
           value={dateDeadline}
           desktopModeMediaQuery=""
+          showDaysOutsideCurrentMonth
+          fixedWeekNumber={5}
           onChange={newValue => {
             setDateDeadline(newValue);
             parentState(newValue);
@@ -44,15 +50,110 @@ export const Calendar = ({ parentState, initial }) => {
           }}
           dayOfWeekFormatter={day => day.slice(0, 2)}
           slotProps={{
-            // desktopPaper: {
-            //   sx: {},
-            // },
-            // day: {
-            //   sx: {},
-            // },
+            desktopPaper: {
+              sx: {
+                borderRadius: '8px',
+                color: 'text.primary',
+                backgroundColor: '#1F1F1F',
+                '& .MuiPickersCalendarHeader-labelContainer': {
+                  position: 'absolute',
+                  left: '50%',
+                  transform: 'translateX(-50%)',
+                  whiteSpace: 'nowrap',
+                },
+                '& .MuiPickersCalendarHeader-label': {
+                  fontSize: '16px',
+                  fontWeight: '500',
+                  letterSpacing: '0.32px',
+                },
+                '& .MuiSvgIcon-root': {
+                  fill: 'text.primary',
+                },
+
+                '& .MuiTypography-root': {
+                  color: 'text.additional',
+                  height: '24px',
+                  marginTop: '14px',
+                  fontSize: '14px',
+                },
+                '& .MuiDayCalendar-weekContainer': {
+                  justifyContent: 'space-between',
+                },
+
+                '& .MuiButtonBase-root': {
+                  color: 'text.primary',
+                  margin: 0,
+                  padding: 0,
+                  fontSize: '14px',
+                  fontStyle: 'normal',
+                  fontWeight: '400',
+                  lineHeight: '18px',
+                },
+                '& .MuiPickersArrowSwitcher-root': {
+                  width: '100%',
+                  justifyContent: 'space-between',
+                },
+                '& .MuiPickersCalendarHeader-switchViewButton': {
+                  display: 'none',
+                },
+                '& .MuiPickersCalendarHeader-root': {
+                  padding: '0',
+                  alignItems: 'baseline',
+                  margin: '0',
+                  borderBottomWidth: '2px',
+                  borderBottomStyle: 'solid',
+                  borderColor: 'divider.card',
+                },
+
+                '& .MuiPickersArrowSwitcher-button': {
+                  height: '21px',
+                },
+
+                '& .MuiDateCalendar-root': {
+                  width: '233px',
+                  height: '254px',
+                  padding: '18px',
+                  boxSizing: 'border-box',
+                  background: 'background.side',
+                },
+
+                '& .MuiPickersDay-hiddenDaySpacingFiller': {
+                  height: '24px',
+                  width: '24px',
+                  minWidth: '24px',
+                  maxWidth: '24px',
+                },
+              },
+            },
+            day: {
+              sx: {
+                height: '24px',
+                width: '24px',
+                minWidth: '24px',
+                maxWidth: '24px',
+                fontSize: '14px',
+
+                '&.MuiPickersDay-root.Mui-selected': {
+                  backgroundColor: 'primary.main',
+                  color: 'text.additional',
+                },
+
+                '&.Mui-selected:hover': {
+                  backgroundColor: 'primary.main',
+                },
+                '&.MuiPickersDay-today': {
+                  borderWidth: 1,
+                  borderColor: 'primary.main',
+                  color: 'text.primary',
+                },
+              },
+            },
             textField: {
               variant: 'outlined',
-              //   sx: {},
+              sx: {
+                '& input': { display: 'none' },
+                '& fieldset': { border: 'none' },
+              },
             },
           }}
         />
