cda1632b764f95e4641d57562e551e3976d44c3b
Denys07748 <denis07748@gmail.com>
2023-08-12 21:10:41 +0300

    add colors to theme

--- src/themes/themes.js
+++ src/themes/themes.js
@@ -101,6 +101,7 @@ export const violetTheme = createTheme({
     background: {
       default: '#ECEDFD',
       header: '#FFF',
+      calendar: '#FFFFFF',
       side: '#5255BC',
       logoIcon: '#ECEDFD',
       helpBanner: 'rgba(236, 237, 253, 0.4)',
@@ -114,6 +115,7 @@ export const violetTheme = createTheme({
     text: {
       primary: '#161616',
       secondary: '#161616B2',
+      additional: '#16161680',
       topTheme: '#161616CC',
       sideMain: '#FFF',
       sideSecond: '#FFFFFF80',
@@ -139,7 +141,7 @@ export const violetTheme = createTheme({
       svgPlusCard: '#161616',
       activeSvgLogout: '#B8BCFD',
     },
-    loader: "#5255BC",
+    loader: '#5255BC',
     ...form,
   },
   ...standardParams,
@@ -159,6 +161,7 @@ export const lightTheme = createTheme({
     background: {
       default: '#F6F6F7',
       header: '#FCFCFC',
+      calendar: '#FFFFFF',
       side: '#FFF',
       logoIcon: '#1F1F1F',
       helpBanner: '#F6F6F7',
@@ -172,6 +175,7 @@ export const lightTheme = createTheme({
     text: {
       primary: '#161616',
       secondary: '#161616B2',
+      additional: '#161616b3',
       topTheme: '#161616CC',
       sideMain: '#161616',
       sideSecond: '#16161680',
@@ -197,7 +201,7 @@ export const lightTheme = createTheme({
       svgPlusCard: '#FFF',
       activeSvgLogout: '#9DC888',
     },
-    loader: "#BEDBB0",
+    loader: '#BEDBB0',
     ...form,
   },
   ...standardParams,
@@ -217,6 +221,7 @@ export const darkTheme = createTheme({
     background: {
       default: '#1F1F1F',
       header: '#161616',
+      calendar: '#1F1F1F',
       side: '#121212',
       logoIcon: '#1F1F1F',
       helpBanner: '#1F1F1F',
@@ -230,6 +235,7 @@ export const darkTheme = createTheme({
     text: {
       primary: '#FFF',
       secondary: '#FFFFFF80',
+      additional: '#ffffff80',
       topTheme: '#FFFFFFCC',
       sideMain: '#FFF',
       sideSecond: '#FFFFFF80',
@@ -255,7 +261,7 @@ export const darkTheme = createTheme({
       svgPlusCard: '#FFF',
       activeSvgLogout: '#9DC888',
     },
-    loader: "#FFFFFF80",
+    loader: '#FFFFFF80',
     ...form,
   },
   ...standardParams,
