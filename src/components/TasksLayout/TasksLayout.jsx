060e730dac8d1be058ce4f7a5fce13179ee8d8f5
Olenteva74 <olenteva74@ukr.net>
2023-08-14 15:28:20 +0300

    add board background

--- src/components/TasksLayout/TasksLayout.jsx
+++ src/components/TasksLayout/TasksLayout.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { TopBar } from 'components/TopBar';
@@ -6,10 +6,23 @@ import { SideBar } from 'components/SideBar';
 import { Container, Box } from '@mui/material';
 import { container } from 'styles';
 import { Loader } from 'components/Loader/Loader';
+import bgImg from 'images/backgrounds.json';
+import { selectBoard} from 'store/boards/selectors';
 
 export const TasksLayout = () => {
+  const board = useSelector(selectBoard);
+  const bgURL  = board.background === 0 ? "" :
+  bgImg.find(bg => bg.id === board.background);
+  const bgImage = {
+    background: {
+      zeroUp: `url("${bgURL?.retina_m}") no-repeat 50% 50%`,
+      tablet: `url("${bgURL?.retina_t}") no-repeat 50% 50%`,
+      desktop: `ull("${bgURL?.retina_d}" no-repeat 50% 50%)`
+    },
+    backgroundSize: "cover"
+  }
   return (
-    <Container sx={container.layout}>
+    <Container sx={{...container.layout, ...bgImage}}>
       <header>
         <TopBar />
       </header>
