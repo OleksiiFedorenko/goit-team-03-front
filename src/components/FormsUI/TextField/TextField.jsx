3fe777a74b73929dab9e62cd4c11f04c35561d15
Vadim Yatskul <vadimajj@gmail.com>
2023-08-14 13:00:36 +0300

    added colors to palette

--- src/components/FormsUI/TextField/TextField.jsx
+++ src/components/FormsUI/TextField/TextField.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import { useField } from 'formik';
+//import { useTheme } from '@mui/material';
 
 const TextFieldWrapper = ({ name, ...otherProps }) => {
   const [field, meta] = useField(name);
-
+  //const theme = useTheme();
   const configTextfield = {
     ...field,
     ...otherProps,
