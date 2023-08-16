965fd38da8b77150f2612bcbcd110f7092bbc854
Denys07748 <denis07748@gmail.com>
2023-08-16 14:12:57 +0300

    delete extra files

--- src/helpers/convertToUnixTime.js
+++
@@ -1,9 +0,0 @@
-import dayjs, { extend } from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
-extend(customParseFormat);
-
-export const convertToUnixTime = dateString => {
-  const format = 'DD-MM-YYYY';
-  const parsedDate = dayjs(dateString, format);
-  return parsedDate.valueOf();
-};
